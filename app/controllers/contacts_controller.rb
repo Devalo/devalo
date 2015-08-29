60179dbfeff44daa7a78e8642f681a9c54ad4ab3
Stephan Valois <stephanvalois@gmail.com>
2015-08-29 19:53:25 +0200

    Laget kontaktskjema og tilhørende actions i controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,5 +2,25 @@ class ContactsController < ApplicationController
    before_filter :set_up_shared_instance_variables
 
    def new
+    @contact = Contact.new
    end
+
+   def create 
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = "Takk for henvendelsen, vi vil svare deg så raskt som mulig."
+      redirect_to kontakt_path
+    else
+      render 'new'
+    end
+   end
+
+
+
+   private
+
+  
+    def contact_params
+      params.require(:contact).permit(:navn, :telefon, :epost, :henvendelse)
+    end
 end
