9611c909b973428ac0c909fbe4136de86d54f0cf
Stephan Valois <stephanvalois@gmail.com>
2015-09-01 20:36:53 +0200

    --DEBUG -- Valideringer for navn, i contacts model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,7 @@ class Contact < ActiveRecord::Base
  # validates :telefon, length: { minimum: 8, maximum: 8}, numericality: true
  # validates :henvendelse, length: { minimum: 10, maximum: 500},
  #                         presence: true
- # validates :navn, presence: true, length: { minimum: 4, maximum: 50 }
+  validates :navn, presence: true, length: { minimum: 4, maximum: 50 }
  # validates :epost, length: { minimum: 4, 
  #                             maximum: 70},
  #                             format: { with: VALID_EMAIL_REGEX }
